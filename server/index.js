0c3b8c157ba7b6346b7c7927d0b2dd847494b641
Shailesh attri <shaileshattri83@gmail.com>
2024-01-29 09:53:20 +0530

    Modified index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {console.log(`Server is running on Port: ${process.env.PORT}`);})
